89365d349bb49cf0935628fa01ad42d1c103ac5a
tim.huang <thkhxm@gmail.com>
2023-03-09 10:22:04 +0800

    移除rpcx-ui项目,新增短key生成

--- tgf/util/uuid.go
+++ tgf/util/uuid.go
@@ -3,6 +3,8 @@ package util
 import (
 	"github.com/bwmarrin/snowflake"
 	"math/rand"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -26,4 +28,35 @@ func initSnowFlake() {
 	source := rand.NewSource(time.Now().UnixNano())
 	ran := rand.New(source)
 	Snowflake, _ = snowflake.NewNode(ran.Int63n(1024))
+}
+
+var codes = []string{"0", "1", "2", "3", "4", "5",
+	"6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I",
+	"J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+	"W", "X", "Y", "Z"}
+
+func GenerateKey(count int) []string {
+	var ()
+	mod := len(codes)
+	res := make([]string, count, count)
+	index := 0
+	for {
+		if index >= count {
+			break
+		}
+		fid := GenerateSnowflakeId()
+		fid = strings.ReplaceAll(fid, "-", "")
+		s := strings.Builder{}
+		for i := 0; i < 8; i++ {
+			subStr := fid[i*4 : i*4+4]
+			x, _ := strconv.ParseInt(subStr, 16, 32)
+			subIndex := x % int64(mod)
+			item := codes[subIndex]
+			s.WriteString(item)
+		}
+		//fmt.Println("code->", s.String())
+		res[index] = s.String()
+		index++
+	}
+	return res
 }
