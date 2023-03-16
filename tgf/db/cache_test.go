dbb5f253ca692fe38234e1498474ab2a4ae0168a
tim.huang <thkhxm@gmail.com>
2023-03-16 17:53:42 +0800

    优化缓存管理

--- tgf/db/cache_test.go
+++ tgf/db/cache_test.go
@@ -2,7 +2,9 @@ package db_test
 
 import (
 	"github.com/thkhxm/tgf/db"
+	"reflect"
 	"testing"
+	"time"
 )
 
 //***************************************************
@@ -44,4 +46,169 @@ func TestDefaultAutoCacheManager(t *testing.T) {
 	//cache_test.go:26: [test] cache first get key 1001 , val 10086
 	//cache_test.go:29: [test] cache set key 1001 , val 10086
 	//cache_test.go:35: [test] cache second get key 1001 , val 10086
+}
+
+func TestAddListItem(t *testing.T) {
+	type args struct {
+		key string
+		val any
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		{"1", args{key: "k1", val: 1}, false},
+		{"2", args{key: "k1", val: 2}, false},
+		{"3", args{key: "k1", val: 3}, false},
+		{"4", args{key: "k1", val: 4}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := db.AddListItem(tt.args.key, tt.args.val); (err != nil) != tt.wantErr {
+				t.Errorf("AddListItem() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
+
+func TestGetList(t *testing.T) {
+	type args struct {
+		key string
+	}
+	type testCase[Res any] struct {
+		name string
+		args args
+		want []Res
+	}
+	tests := []testCase[string]{
+		{"1", args{key: "k1"}, []string{"4", "3", "2", "1"}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := db.GetList[string](tt.args.key); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetList() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDel(t *testing.T) {
+	type args struct {
+		key string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db.Del(tt.args.key)
+		})
+	}
+}
+
+func TestDelNow(t *testing.T) {
+	type args struct {
+		key string
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db.DelNow(tt.args.key)
+		})
+	}
+}
+
+func TestGetMap(t *testing.T) {
+	//type args struct {
+	//	key string
+	//}
+	//type testCase[Key cacheKey, Val any] struct {
+	//	name        string
+	//	args        args
+	//	wantRes     map[Key]Val
+	//	wantSuccess bool
+	//}
+	//tests := []testCase[ /* TODO: Insert concrete types here */ ]{
+	//	// TODO: Add test cases.
+	//}
+	//for _, tt := range tests {
+	//	t.Run(tt.name, func(t *testing.T) {
+	//		gotRes, gotSuccess := db.GetMap(tt.args.key)
+	//		if !reflect.DeepEqual(gotRes, tt.wantRes) {
+	//			t.Errorf("GetMap() gotRes = %v, want %v", gotRes, tt.wantRes)
+	//		}
+	//		if gotSuccess != tt.wantSuccess {
+	//			t.Errorf("GetMap() gotSuccess = %v, want %v", gotSuccess, tt.wantSuccess)
+	//		}
+	//	})
+	//}
+}
+
+func TestPutMap(t *testing.T) {
+	//type args[Key cacheKey, Val any] struct {
+	//	key     string
+	//	field   db.Key
+	//	val     db.Val
+	//	timeout time.Duration
+	//}
+	//type testCase[Key cacheKey, Val any] struct {
+	//	name string
+	//	args args[Key, Val]
+	//}
+	//tests := []testCase[ /* TODO: Insert concrete types here */ ]{
+	//	// TODO: Add test cases.
+	//}
+	//for _, tt := range tests {
+	//	t.Run(tt.name, func(t *testing.T) {
+	//		db.PutMap(tt.args.key, tt.args.field, tt.args.val, tt.args.timeout)
+	//	})
+	//}
+}
+
+func TestSet(t *testing.T) {
+	type args struct {
+		key     string
+		val     any
+		timeout time.Duration
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			db.Set(tt.args.key, tt.args.val, tt.args.timeout)
+		})
+	}
+}
+
+func TestSetList(t *testing.T) {
+	//type args[Val any] struct {
+	//	key     string
+	//	l       []Val
+	//	timeout time.Duration
+	//}
+	//type testCase[Val any] struct {
+	//	name string
+	//	args args[Val]
+	//}
+	//tests := []testCase[ /* TODO: Insert concrete types here */ ]{
+	//	// TODO: Add test cases.
+	//}
+	//for _, tt := range tests {
+	//	t.Run(tt.name, func(t *testing.T) {
+	//		db.SetList(tt.args.key, tt.args.l, tt.args.timeout)
+	//	})
+	//}
 }
